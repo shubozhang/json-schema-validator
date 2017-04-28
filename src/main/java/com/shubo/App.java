e626e2ca6b268008d762b2a83f47ecb7d8637598
K26511 <shubo.zhang@finra.org>
2017-04-28 09:19:46 -0400

    start project

---
+++ src/main/java/com/shubo/App.java
@@ -0,0 +1,25 @@
+package com.shubo;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ListProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import java.io.IOException;
+
+
+public class App {
+
+    public ListProcessingReport validateJson(String schemaPath, String jsonString) throws ProcessingException, IOException {
+        JsonNode fsJsonSchema = JsonLoader.fromPath(schemaPath);
+        final JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+        final JsonSchema schema = factory.getJsonSchema(fsJsonSchema);
+
+
+        JsonNode fsJson = JsonLoader.fromString(jsonString);
+        ListProcessingReport report = (ListProcessingReport) schema.validate(fsJson);
+
+        return report;
+    }
+}
